b85452424edc89b6afe34e0b76070a2b1efecf80
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-01 19:17:59 +0300

    kategori modeli oluşturuldu

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blog.models import KategoriModel
+
+admin.site.register(KategoriModel)
 
 # Register your models here.
---
+++ blog/models/kategori.py
@@ -0,0 +1,14 @@
+#Yazi -> Yorum -> Kategori
+
+from django.db import models
+from autoslug import AutoSlugField
+
+class KategoriModel(models.Model):
+    isim = models.CharField(max_length=30, blank=False, null=False)
+    slug = AutoSlugField(populate_from='isim', unique=True)
+
+    class Meta:
+        db_table = 'kategori'
+
+    def __str__(self):
+        return self.isim
