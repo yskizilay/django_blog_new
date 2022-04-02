cb89720d5193487ce799e01cadb8cf1437a8d7f4
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-03 02:20:30 +0300

    account ve admin_avatar ayarları

---
+++ account/models.py
@@ -0,0 +1,15 @@
+from tabnanny import verbose
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUserModel(AbstractUser):
+    avatar = models.ImageField(upload_to = 'avatar/', blank= True, null=True)
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'KUllanıcı'
+        verbose_name_plural = 'Kullanıcılar'
+
+    def __str__(self):
+        return self.username
--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'blog',
+    #third party
     'ckeditor'
 ]
 
@@ -105,3 +107,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.CustomUserModel'
