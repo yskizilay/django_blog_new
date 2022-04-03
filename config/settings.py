36c288e8b3a9b0cffee34ed8d30b54d000811b5e
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-03 11:23:26 +0300

    media_url ayarları

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,3 +111,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'account.CustomUserModel'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
