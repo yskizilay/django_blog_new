c1a5096d2fbc663875c380e965dac911aade2736
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-03 17:09:16 +0300

    listeleme_sayfalama_bitti

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import iletisim, anasayfa
+from blog.views import iletisim, anasayfa, kategori, yazilarim
 
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
-    path('iletisim', iletisim, name='iletisim')
+    path('iletisim', iletisim, name='iletisim'),
+    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yazilarim', yazilarim, name='yazilarim')
 ]
