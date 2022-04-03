465aafbbf014c8326845c66852f4de9fa53bf0eb
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-03 22:24:10 +0300

    create_form_valid

---
+++ blog/forms/iletisim.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django import forms
+
+
+class IletisimForm(forms.Form):
+    email = forms.EmailField(max_length=100)
+    isim_soyisim = forms.CharField(max_length=25)
+    mesaj = forms.CharField(widget=forms.Textarea)
--- blog/views/iletisim.py
+++ blog/views/iletisim.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog.forms import IletisimForm
+from blog.models import IletisimModel
 
 def iletisim(request):
+
+    form = IletisimForm()
+    if request.method == 'POST':
+        form = IletisimForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+                iletisim = IletisimModel()
+                iletisim.email = form.cleaned_data['email']
+                iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
+                iletisim.mesaj = form.cleaned_data['mesaj']
+                iletisim.save()
+                return redirect('anasayfa')
+                
     context = {
-        'sayi' : 6
+
+        'form': form
     }
 
     return render(request, 'pages/iletisim.html', context=context)
