91dff69e452e38b5f11f2379b0b5f7a94541e885
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-04 00:25:02 +0300

    formmodel

--- blog/forms/iletisim.py
+++ blog/forms/iletisim.py
@@ -1,8 +1,11 @@
-from unittest.util import _MAX_LENGTH
+
 from django import forms
+from blog.models import IletisimModel
+
+class IletisimForm(forms.ModelForm):
+    class Meta:
+        model = IletisimModel
+        fields =('isim_soyisim', 'email', 'mesaj')
 
 
-class IletisimForm(forms.Form):
-    email = forms.EmailField(label='E-Posta', max_length=100)
-    isim_soyisim = forms.CharField(label= 'Ad Soyad', max_length=25)
-    mesaj = forms.CharField(label= 'Mesajınız', widget=forms.Textarea)
+    
--- blog/views/iletisim.py
+++ blog/views/iletisim.py
@@ -9,13 +9,9 @@ def iletisim(request):
         form = IletisimForm(request.POST)
         print(request.POST)
         if form.is_valid():
-                iletisim = IletisimModel()
-                iletisim.email = form.cleaned_data['email']
-                iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
-                iletisim.mesaj = form.cleaned_data['mesaj']
-                iletisim.save()
+                form.save()
                 return redirect('anasayfa')
-                
+
     context = {
 
         'form': form
