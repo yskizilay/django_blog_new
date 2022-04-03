f6fd1ac5f39e50d34e1308ee2c30ee4bc441acb9
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-03 15:11:25 +0300

    Ana sayfa paginator

--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from blog.models import YazilarModel
+from django.core.paginator import Paginator
 
 
 
 def anasayfa(request):
-    yazilar = YazilarModel.objects.all()
+    yazilar = YazilarModel.objects.order_by('-id')
+    sayfa = request.GET.get('sayfa')
+    paginator = Paginator(yazilar, 1)
+
     return render(request, 'pages/anasayfa.html', context = {
-        'yazilar': yazilar
+        'yazilar': paginator.get_page(sayfa)
     })
