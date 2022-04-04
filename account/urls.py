791b9cfc11178e1d9b2ed809f99499ee2c7c56da
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-04 22:49:26 +0300

    giris_yap

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from account.views import cikis,sifre_degistir,profil_guncelle,kayit
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+urlpatterns = [ 
+    path('giris', auth_views.LoginView.as_view(
+        template_name = 'pages/giris.html'
+    ), name='giris' ),
     path('cikis', cikis, name='cikis'),
     path('sifre-degistir', sifre_degistir, name='sifre-degistir'),
     path('profil-guncelle', profil_guncelle, name='profil-guncelle'),
-    path('kayit', kayit, name='kayit')
-]
+    path('kayit', kayit, name='kayit'), 
+    ]
--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +118,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL='/'
