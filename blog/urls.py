019e927f449787a5c9ca8b31de03ff84a65b6719
yskizilay <yunus.kizilay@ogr.sakarya.edu.tr>
2022-04-04 12:38:01 +0300

    yorum_sil

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import iletisim, anasayfa, kategori, yazilarim,detay, yazi_ekle,yazi_guncelle,yazi_sil
+from blog.views import iletisim, anasayfa, kategori, yazilarim,detay, yazi_ekle,yazi_guncelle,yazi_sil,yorum_sil
 
 
 urlpatterns = [
@@ -11,5 +11,6 @@ urlpatterns = [
     path('yazi-ekle', yazi_ekle, name='yazi-ekle'),
     path('yazi-guncelle/<slug:slug>', yazi_guncelle, name='yazi-guncelle'),
     path('yazi-sil/<slug:slug>', yazi_sil, name='yazi-sil'),
+    path('yorum-sil/<int:id>', yorum_sil, name='yorum-sil'),
 
 ]
---
+++ blog/views/yorum_sil.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from blog.models import YorumModel
+from django.shortcuts import get_object_or_404, redirect
+
+@login_required(login_url='/')
+def yorum_sil(request, id):
+    yorum = get_object_or_404(YorumModel, id=id)
+    if yorum.yazan == request.user or yorum.yazi.yazar == request:
+        yorum.delete()
+        return redirect('detay', slug=yorum.yazi.slug)
+    return redirect('anasayfa')
